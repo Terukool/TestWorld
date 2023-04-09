9da51c40ac69af997b0368e3c8496e3e7f70dcc0
Terukool <taabkl@gmail.com>
2023-04-09 21:44:15 +0300

    progress

--- angular-13-app/src/app/select-two/select-two.component.ts
+++ angular-13-app/src/app/select-two/select-two.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+
+import { CdkOverlayOrigin, OverlayRef } from '@angular/cdk/overlay';
+import { Component, OnInit, ViewChild, AfterViewInit, HostListener, ChangeDetectorRef, ChangeDetectionStrategy, ApplicationRef, ElementRef } from '@angular/core';
+import { MAT_SELECT_CONFIG, MatSelect } from '@angular/material/select';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 interface Item {
   id: number;
@@ -10,10 +14,20 @@ interface Item {
   selector: 'app-select-two',
   templateUrl: './select-two.component.html',
   styleUrls: ['./select-two.component.scss'],
+  providers: [
+    {
+      provide: MAT_SELECT_CONFIG,
+      useValue: { overlayPanelClass: 'custom-panel' }
+    }
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectTwoComponent implements OnInit {
+export class SelectTwoComponent implements OnInit, AfterViewInit {
 
-  public selectedItem?: Item; 
+  @ViewChild('select') select?: MatSelect;
+  @ViewChild('target') target?: ElementRef;
+
+  public selectedItem?: Item;
   public items: Item[] = [
     { id: 1, name: 'Item 1', category: 'Category 1' },
     { id: 2, name: 'Item 2', category: 'Category 1' },
@@ -22,11 +36,35 @@ export class SelectTwoComponent implements OnInit {
     { id: 5, name: 'Item 5', category: 'Category 2' },
     { id: 6, name: 'Item 6' },
   ];
+  public changed: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
 
   constructor() { }
 
+
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    if (!this.select || !this.target) {
+      return;
+    }
+
+    this.select._positions = [];
+
+    //@ts-ignore
+    const selectOverlay = this.select._overlayDir;
+
+    selectOverlay.origin = this.target;
+
+    const width = this.target.nativeElement.getBoundingClientRect().width;
+
+    // get the bounding box of target
+    selectOverlay.minWidth = width;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.select?.stateChanges.next();
+  }
 }
