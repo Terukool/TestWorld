5ef11cb05e98a4ced4f5c4c643b39d128086d63d
Terukool <taabkl@gmail.com>
2023-04-11 23:13:33 +0300

    Disgusting Position Detection

--- angular-13-app/src/app/select-two/select-two.component.ts
+++ angular-13-app/src/app/select-two/select-two.component.ts
@@ -1,8 +1,8 @@
 
 import { CdkConnectedOverlay, CdkOverlayOrigin, OverlayRef, ViewportRuler } from '@angular/cdk/overlay';
-import { Component, OnInit, ViewChild, AfterViewInit, HostListener, ChangeDetectorRef, ChangeDetectionStrategy, ApplicationRef, ElementRef, OnChanges } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, HostListener, ChangeDetectorRef, ChangeDetectionStrategy, ApplicationRef, ElementRef, OnChanges, ContentChild, NgZone } from '@angular/core';
 import { MAT_SELECT_CONFIG, MatSelect } from '@angular/material/select';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, delay, distinctUntilChanged, map, of, tap } from 'rxjs';
 
 interface Item {
   id: number;
@@ -26,6 +26,11 @@ export class SelectTwoComponent implements AfterViewInit {
 
   @ViewChild('select') select?: MatSelect;
 
+  private get overlayRef(): OverlayRef | undefined {
+    //@ts-ignore
+    return this.select?._overlayDir?.overlayRef;
+  }
+
   public selectedItem?: Item[] = [];
   public items: Item[] = [
     { id: 1, name: 'Item 1', category: 'Category 1' },
@@ -35,10 +40,10 @@ export class SelectTwoComponent implements AfterViewInit {
     { id: 5, name: 'Item 5', category: 'Category 2' },
     { id: 6, name: 'Item 6' },
   ];
-  public changed: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
+  constructor(protected _viewportRuler: ViewportRuler, protected _changeDetectorRef: ChangeDetectorRef, private zone: NgZone) { }
 
-  constructor(protected _viewportRuler: ViewportRuler, protected _changeDetectorRef: ChangeDetectorRef) { }
+  public positionStrategy$!: Observable<string>;
 
   ngAfterViewInit(): void {
     if (!this.select) {
@@ -48,16 +53,42 @@ export class SelectTwoComponent implements AfterViewInit {
     this.select._positions = [];
 
     const resizeObserver = new ResizeObserver(() => {
-      //@ts-ignore
-      const overlayRef = this.select?._overlayDir?.overlayRef;
-
-      if (this.select?.panelOpen && overlayRef) {
-        overlayRef.updatePosition();
-        overlayRef.updateSize({ minWidth: this.select?.trigger?.nativeElement?.offsetWidth });
+      if (this.select?.panelOpen && this.overlayRef) {
+        this.overlayRef.updatePosition();
+        this.overlayRef.updateSize({ minWidth: this.select?.trigger?.nativeElement?.offsetWidth });
       }
     })
 
     resizeObserver.observe(this.select.trigger.nativeElement);
+
+    this.trackOverlayOpeningPosition();
   }
 
+  private trackOverlayOpeningPosition() {
+    //@ts-ignore
+    const positionChange$ = this.select?._overlayDir?.positionChange.asObservable();
+
+    if (!positionChange$) {
+      return;
+    }
+
+    this.positionStrategy$ = positionChange$.pipe(
+      delay(0),
+      map(({ connectionPair: { originY } }) => {
+        return originY;
+      }),
+      distinctUntilChanged(),
+      map((originY) => {
+        const CLASS_PREFIX = 'open-';
+        const openedAt = originY === 'top' ? 'above' : 'below';
+
+        return `${CLASS_PREFIX}${openedAt}`;
+      })
+    );
+
+
+    this.positionStrategy$.pipe(delay(0)).subscribe(() => {
+      this._changeDetectorRef.detectChanges();
+    }); // This is disgusting, but it's the only way I could get it to work, not sure why I need it
+  }
 }
