65c77ce94f563f75ed4bb9adc69d0a3130b0b923
Terukool <taabkl@gmail.com>
2023-05-19 22:08:52 +0300

    laughing from pain

--- another-monerepo-idk-why/apps/regular-webpack/src/app/app.controller.ts
+++ another-monerepo-idk-why/apps/regular-webpack/src/app/app.controller.ts
@@ -1,11 +1,12 @@
 import { Delete, Get, Param, Post } from '@nestjs/common';
 
 import { HeliosController } from './helios-controller';
+import { ApiParam } from '@nestjs/swagger';
 
 @HeliosController(':baseId/Workspaces/:id')
 export class AppController {
-  @Get('1:/:anotherId')
-  getDataById(@Param('id') id: string, @Param('baseId') baseId: string, @Param('anotherId') anotherId: string) {
+  @Get('1/:OptionalId3?')
+  getDataById(@Param('id') id: string, @Param('baseId') baseId: string, @Param('OptionalId3') anotherId: number | undefined) {
     console.log('id', id);
     return id + baseId + anotherId + ' found';
   }
